8fe1a8d3562d7a24405204968e7c9b10df0b4d8c
卓同学 <lacklock@gmail.com>
2016-12-31 16:38:23 +0800

    project init

---
+++ BusinessA/BusinessA.h
@@ -0,0 +1,19 @@
+//
+//  BusinessA.h
+//  BusinessA
+//
+//  Created by 卓同学 on 2016/12/31.
+//  Copyright © 2016年 卓同学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BusinessA.
+FOUNDATION_EXPORT double BusinessAVersionNumber;
+
+//! Project version string for BusinessA.
+FOUNDATION_EXPORT const unsigned char BusinessAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BusinessA/PublicHeader.h>
+
+
---
+++ BusinessB/BusinessB.h
@@ -0,0 +1,19 @@
+//
+//  BusinessB.h
+//  BusinessB
+//
+//  Created by 卓同学 on 2016/12/31.
+//  Copyright © 2016年 卓同学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BusinessB.
+FOUNDATION_EXPORT double BusinessBVersionNumber;
+
+//! Project version string for BusinessB.
+FOUNDATION_EXPORT const unsigned char BusinessBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BusinessB/PublicHeader.h>
+
+
